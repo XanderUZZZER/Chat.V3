2785614069f154a5a637f498b7c9da4bb4ca79ab
XanderUZZZER <xander_uzzzer@mail.ru>
2017-06-26 01:02:02 +0300

    1

--- Chat/ServerConnectedClient.cs
+++ Chat/ServerConnectedClient.cs
@@ -39,7 +39,6 @@ namespace Chat
                     writer.Write((int)Requests.ConnectionOk);
                     writer.Flush();
                     WorkWithClient();
-
                 }
             }
         }
